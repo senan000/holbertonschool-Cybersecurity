c932e89299e5b16fcd064e11a02d809c2ca18544
senan000 <13737@holbertonstudents.com>
2026-09-11 12:59:14 -0400

    tasks

--- network_security/2x00_network_fundamentals/3-ip2bin.sh
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-IP=$1
-A=$(echo "$IP" | cut -d. -f1)
-B=$(echo "$IP" | cut -d. -f2)
-C=$(echo "$IP" | cut -d. -f3)
-D=$(echo "$IP" | cut -d. -f4)
-A=$(echo "obase=2; $A" | bc)
-B=$(echo "obase=2; $B" | bc)
-C=$(echo "obase=2; $C" | bc)
-D=$(echo "obase=2; $D" | bc)
-A=$(printf "%08d" "$A")
-B=$(printf "%08d" "$B")
-C=$(printf "%08d" "$C")
-D=$(printf "%08d" "$D")
-echo "$A.$B.$C.$D"
+echo "$(printf '%08d' "$(echo "obase=2; $(echo "$1" | cut -d. -f1)" | bc)").$(printf '%08d' "$(echo "obase=2; $(echo "$1" | cut -d. -f2)" | bc)").$(printf '%08d' "$(echo "obase=2; $(echo "$1" | cut -d. -f3)" | bc)").$(printf '%08d' "$(echo "obase=2; $(echo "$1" | cut -d. -f4)" | bc)")"

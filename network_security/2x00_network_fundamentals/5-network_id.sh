e3cfb4ede41fd21ba7b2ae1eea9e84c58dd1d207
senan000 <13737@holbertonstudents.com>
2026-09-11 13:01:09 -0400

    tasks

--- network_security/2x00_network_fundamentals/5-network_id.sh
+++ network_security/2x00_network_fundamentals/5-network_id.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-IFS=. read -r a b c d <<< "$1"
-IFS=. read -r w x y z <<< "$2"
-echo "$((a & w)).$((b & x)).$((c & y)).$((d & z))"
+IFS=. read -r a b c d <<< "$1"; IFS=. read -r w x y z <<< "$2"; printf "%s\n" "$((a & w)).$((b & x)).$((c & y)).$((d & z))"

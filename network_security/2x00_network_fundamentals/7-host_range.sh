919bbbf7dd9eba0afed6318d080709b6c5d47c13
senan000 <13737@holbertonstudents.com>
2026-09-11 13:02:55 -0400

    tasks

---
+++ network_security/2x00_network_fundamentals/7-host_range.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+IFS=. read -r a b c d <<< "$1"; n=$((32 - $2)); ip=$(( (a << 24) | (b << 16) | (c << 8) | d )); mask=$((0xffffffff << n & 0xffffffff)); net=$((ip & mask)); first=$((net + 1)); last=$((net + (1 << n) - 2)); printf "%d.%d.%d.%d - %d.%d.%d.%d" "$((first >> 24 & 255))" "$((first >> 16 & 255))" "$((first >> 8 & 255))" "$((first & 255))" "$((last >> 24 & 255))" "$((last >> 16 & 255))" "$((last >> 8 & 255))" "$((last & 255))"

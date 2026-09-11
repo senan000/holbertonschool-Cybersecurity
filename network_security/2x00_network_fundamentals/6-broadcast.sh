065853f3625899c0dcc5f2984479cda76330102d
senan000 <13737@holbertonstudents.com>
2026-09-11 13:02:26 -0400

    tasks

--- network_security/2x00_network_fundamentals/6-broadcast.sh
+++ network_security/2x00_network_fundamentals/6-broadcast.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IFS=. read -r a b c d <<< "$1"; IFS=. read -r w x y z <<< "$2"; printf "%s\n" "$((a | (255 ^ w))).$((b | (255 ^ x))).$((c | (255 ^ y))).$((d | (255 ^ z)))"
+IFS=. read -r a b c d <<< "$1"; IFS=. read -r w x y z <<< "$2"; printf "%s" "$((a | (255 ^ w))).$((b | (255 ^ x))).$((c | (255 ^ y))).$((d | (255 ^ z)))"
